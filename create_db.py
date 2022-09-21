a1308fcc005c83cdc8bb3785058e80682f058354
sagsshakya <sags.shakya@gmail.com>
2022-09-21 14:05:47 +0545

    set difference

--- create_db.py
+++ create_db.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 
-from utils import dump_json
+from utils import dump_json, load_teachers
 
 root_dir = 'data\\intermidiate'
 input_filenames = os.listdir(root_dir)
@@ -13,4 +13,13 @@ keys = [item.split(".")[0][-2:] for item in input_filenames]
     ## key_grade : dict(routine)
 df_dict = {KK: pd.read_csv(FILEPATH, delimiter = "\t").to_dict() for KK, FILEPATH in zip(keys, input_filepaths)}
 
+# Serialize to JSON.
 dump_json(df_dict)
+
+# Update teachers - universal.
+teachers_coll = set()
+for dictionary in df_dict.values():
+    teachers_set = load_teachers(pd.DataFrame(dictionary))
+    teachers_coll = teachers_coll.union(teachers_set)
+
+print(teachers_coll.difference({'void'}))
